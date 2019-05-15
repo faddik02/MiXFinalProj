63bffa6faf0a3e1d254ffc526bb2c0e15a2e2b96
faddik02 <faddik02@gmail.com>
2019-05-15 20:52:56 +0200

    All of the files
    The upload of the whole project since we used unity collaboration

---
+++ FinalMixProjectO/Assets/Scripts/PlayerColision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+    private ArrayList numbers;
+    [SerializeField]
+    int requiredNumbers;
+
+    void OnCollisionEnter(Collision collisonInfo)
+    {
+        var objectName = collisonInfo.collider.name;
+        var objectIntegerName = int.Parse(objectName);
+
+        if(objectName == "1")
+        {
+            numbers.Add(objectIntegerName);
+            Destroy(collisonInfo.gameObject);
+        }
+
+        if(objectName == "2")
+        {
+            numbers.Add(objectIntegerName);
+            Destroy(collisonInfo.gameObject);
+        }    
+
+    }
+}
